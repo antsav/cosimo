d4b060c26fcbe7275b22c2944427ff4dfacf7699
Anton <antsav@gmail.com>
2016-06-01 20:43:15 -0700

    watch added

---
+++ tasks/watch.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+module.exports = function less(grunt) {
+	// Load task
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	// Options
+	return {
+		watch: {
+          files: [ "src/**/*.*", "./index.html"],
+          tasks: [ 'default' ]
+        }
+	};
+};
